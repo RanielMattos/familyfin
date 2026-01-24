dbbacb6b15a596ef87d6889e54e14d5d29c1ee64
RanielMattos <ranielmattos@protonmail.com>
2026-01-23 23:17:29 -0300

    chore: add root healthcheck route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaxonomyController;
 use App\Http\Controllers\PlanpagController;
 
+Route::get('/', function () {
+    return response()->json([
+        'ok' => true,
+        'app' => 'FamilyFin',
+    ]);
+});
+
 Route::get('/planpag', [PlanpagController::class, 'index']);
 
 Route::get('/taxonomia', [TaxonomyController::class, 'index']);
